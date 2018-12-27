5b5fd7ca202dc3368f863c862c66cb326e85c600
shguijj@cn.ibm.com <shguijj@cn.ibm.com>
2018-12-27 21:34:05 +0800

    header and tabbar

---
+++ Webpack/webpack4-vue-demo/src/app.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class='app-container'>
+
+    <!-- Header -->
+
+    <nav class="navbar navbar-dark bg-primary fixed-top">
+      <span class="navbar-brand nav-header">
+        Vue2.5.21 Project Demo
+      </span>
+    </nav>
+
+    <!-- Router -->
+
+    <!-- 底部Tapbar -->
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-bottom">
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav nav-bottom">
+      <li class="nav-item active">
+        <a class="nav-link" href="#"><i class="fas fa-home"></i>Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><i class="fas fa-envelope"></i>Message</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><i class="far fa-address-book"></i>Contacts</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><i class="fas fa-cogs"></i>Setting</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+    
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+div.app-container {
+  padding-top: 56px;
+}
+
+span.nav-header, ul.nav-bottom {
+  margin: 0 auto;
+}
+
+
+</style>
+
--- Webpack/webpack4-vue-demo/src/index.js
+++ Webpack/webpack4-vue-demo/src/index.js
@@ -1,34 +1,18 @@
-//import bootstrap style
 import 'bootstrap/dist/css/bootstrap.min.css'
-
-//Import font-awesome
 import '@fortawesome/fontawesome-free/css/all.css'
-
-//Custom css
 import "./css/index.css"
 
-//Import vue - Style-1
-//这个是runtime only的包，不能编译
-//import Vue from 'vue'
-
-//这种方式导入最全的vue的包，但是不推荐这么使用！！！！ Style-1
-//import Vue from 'vue/dist/vue.common.dev'
-
-//Import vue Style-3, 在webpack.config.js 配置resolve，指定vue的别名。 但是不推荐这么使用！！！！
-
-//推荐使用runtime的包，结合Vue render函数
 import Vue from 'vue'
 
-//Import login.vue
 
-import login from './login.vue'
+import app from './app.vue'
 
 var vm = new Vue({
   el: '#app',
   data: {
     msg: 'Vue-v2.5.21 based webpack4.'
   },
-  render: (c) => c(login)  //c-> createElement
+  render: (c) => c(app)  //c-> createElement
    
 });
 
