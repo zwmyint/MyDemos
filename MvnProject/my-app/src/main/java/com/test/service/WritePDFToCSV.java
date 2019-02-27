3484bde3bb0904c252f32407059423fa7c44a972
shguijj <shguijj@cn.ibm.com>
2019-02-27 21:54:13 +0800

    format pom.xml

--- MvnProject/my-app/src/main/java/com/test/service/WritePDFToCSV.java
+++ MvnProject/my-app/src/main/java/com/test/service/WritePDFToCSV.java
@@ -12,33 +12,35 @@ import org.slf4j.LoggerFactory;
 
 public class WritePDFToCSV {
 
-  private static final Logger logger = LoggerFactory.getLogger(WritePDFToCSV.class);
+    private static final Logger logger = LoggerFactory.getLogger(WritePDFToCSV.class);
 
-  public static void main(String[] args) {
-    String rootPath = args[0];
-    File root = new File(rootPath);
-    if (!root.exists()) {
-      logger.error("Invalid pdf dir error!!");
-      return;
+    public static void main(String[] args) {
+        String rootPath = args[0];
+        File root = new File(rootPath);
+        if (!root.exists()) {
+            logger.error("Invalid pdf dir error!!");
+            return;
+        }
+        List<Map<String, String>> result = new ArrayList<Map<String, String>>();
+        Stream.of(root.listFiles()).filter(file -> file.getName().contains(".pdf")).forEach(pdf -> {
+            ReadPDFWithPDFBox readPDFWithPDFBox = new ReadPDFWithPDFBox(pdf);
+            result.add(readPDFWithPDFBox.extractText());
+        });
+
+        StringBuffer csvContent = new StringBuffer();
+        csvContent.append("姓名").append(",").append("发票代码").append(",")
+                .append("发票号码").append(",").append("金额").append("\r\n");
+
+        result.stream().forEach(pdfContent -> {
+            csvContent.
+                    append(pdfContent.get("Name")).append(",").
+                    append(pdfContent.get("InvoiceCode")).append(",").
+                    append(pdfContent.get("InvoiceNum")).append(",").
+                    append(pdfContent.get("Money")).append("\r\n");
+        });
+        File csv = new File(rootPath + File.separator + "result.csv");
+        FileUtil.writeFile(csvContent.toString(), csv);
+
     }
-    List<Map<String, String>> result = new ArrayList<Map<String, String>>();
-    Stream.of(root.listFiles()).filter(file -> file.getName().contains(".pdf")).forEach(pdf -> {
-      ReadPDFWithPDFBox readPDFWithPDFBox = new ReadPDFWithPDFBox(pdf);
-      result.add(readPDFWithPDFBox.extractText());
-    });
-
-    StringBuffer csvContent = new StringBuffer();
-
-    result.stream().forEach(pdfContent -> {
-      csvContent.
-          append(pdfContent.get("Name")).append(",").
-          append(pdfContent.get("InvoiceCode")).append(",").
-          append(pdfContent.get("InvoiceNum")).append(",").
-          append(pdfContent.get("Money")).append("\r\n");
-    });
-    File csv = new File(rootPath + File.separator + "result.csv");
-    FileUtil.writeFile(csvContent.toString(), csv);
-
-  }
 
 }
