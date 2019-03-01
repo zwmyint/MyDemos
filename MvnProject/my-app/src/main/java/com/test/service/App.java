58bf6e09d42ffeea94b62bd4850448ad1fc4214c
shguijj <shguijj@cn.ibm.com>
2019-03-01 17:00:47 +0800

    progress bar

--- MvnProject/my-app/src/main/java/com/test/service/App.java
+++ MvnProject/my-app/src/main/java/com/test/service/App.java
@@ -3,6 +3,11 @@ package com.test.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static org.fusesource.jansi.Ansi.Color.BLUE;
+import static org.fusesource.jansi.Ansi.Color.DEFAULT;
+import static org.fusesource.jansi.Ansi.Color.GREEN;
+import static org.fusesource.jansi.Ansi.ansi;
+
 /**
  * Hello world!
  */
@@ -10,23 +15,62 @@ public class App {
 
   private static final Logger logger = LoggerFactory.getLogger(App.class);
 
-  public static void main(String[] args) throws Exception {
-    System.out.print("Progress:");
-    for (int i = 1; i <= 100; i++) {
-      System.out.print(i + "%");
-      Thread.sleep(200);
 
-      for (int j = 0; j <= String.valueOf(i).length(); j++) {
+  public static void main(String[] args) throws Exception {
+    for (int i = 1; i <= 100; i++) {
+      String progress = buildProgressBar(i, 100);
+      System.out.print(progress);
+      Thread.sleep(100);
+      for (int j = 1; j <= progress.length(); j++) {
         System.out.print("\b");
       }
-
       if (i == 100) {
-        for (int j = 0; j <= String.valueOf("Progress:").length(); j++) {
+        for (int j = 1; j <= progress.length(); j++) {
           System.out.print("\b");
         }
-        System.out.print("Completed!!");
+        System.out.print(buildDone(i, 100));
       }
     }
-    System.out.println();
   }
+
+  private static String buildDone(int cellNum, int sum) {
+    String cell = "█";
+    String colorCells = "";
+    StringBuffer sb = new StringBuffer();
+    String colorTitle = ansi().eraseScreen().fg(BLUE).a("Completed").toString();
+    String progress =
+        ansi().eraseScreen().fg(DEFAULT).a("<").toString() + colorTitle + ansi().eraseScreen()
+            .fg(DEFAULT).a(":").toString() + cellNum + "%> ";
+    for (int i = 0; i < cellNum; i++) {
+      sb.append(cell);
+    }
+    colorCells = ansi().eraseScreen().fg(GREEN).a(sb.toString()).toString();
+    return progress + colorCells;
+  }
+
+  private static String buildProgressBar(int cellNum, int sum) {
+    String cell = "█";
+    String empty = "░";
+    String progressBar = "";
+    String colorCells = "";
+    String colorEmptys = "";
+    StringBuffer sb = new StringBuffer();
+
+    String colorTitle = ansi().eraseScreen().fg(BLUE).a("Progress").toString();
+    String progress =
+        ansi().eraseScreen().fg(DEFAULT).a("<").toString() + colorTitle + ansi().eraseScreen()
+            .fg(DEFAULT).a(":").toString() + String.format("%02d", cellNum) + "%> ";
+    for (int i = 0; i < cellNum; i++) {
+      sb.append(cell);
+    }
+    colorCells = ansi().eraseScreen().fg(GREEN).a(sb.toString()).toString();
+    sb.setLength(0);
+    for (int j = 0; j < sum - cellNum; j++) {
+      sb.append(empty);
+    }
+    colorEmptys = ansi().eraseScreen().fg(DEFAULT).a(sb.toString()).toString();
+    progressBar = progress + colorCells + colorEmptys;
+    return progressBar;
+  }
+
 }
--- MvnProject/my-app/src/main/java/com/test/service/JansiUtil.java
+++ MvnProject/my-app/src/main/java/com/test/service/JansiUtil.java
@@ -8,10 +8,21 @@ import static org.fusesource.jansi.Ansi.ansi;
 
 public class JansiUtil {
 
+  public static void paintProgress(String description, Integer length) {
+    if (description == null) {
+      description = "PROGRESS";
+    }
+    if (length == null) {
+      length = 28;
+    }
+
+
+  }
+
   public static void main(String[] args) {
     int i = 1 % 7;
     System.out.print(ansi().eraseScreen().fg(RED).a(i).fg(YELLOW).a(" World").reset());
-    System.out.print(ansi().eraseScreen().fg(RED).a(i).bg(GREEN).a(" World"));
+    System.out.print(ansi().eraseScreen().fg(RED).bg(GREEN).a(" World"));
     System.out.println("\033[30;4m" + "我滴个颜什" + "\033[0m");
     System.out.println("\033[31;4m" + "我滴个颜什" + "\033[0m");
   }
