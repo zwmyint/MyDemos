47d349c1eab44753f5c9a12e1e0fe3843406b0db
evliess <jiajia_yuwei@163.com>
2019-06-04 21:54:49 +0800

    add dependencies

---
+++ SpringBoot/springboot-microservice-handon/src/test/java/com/wait/play/ReactorTestCase.java
@@ -0,0 +1,13 @@
+package com.wait.play;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ReactorTestCase {
+
+
+  private List<String> words = Arrays
+      .asList("the", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog");
+
+
+}
