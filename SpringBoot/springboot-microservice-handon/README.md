aeec227f7b9377b5b0b1bc02c092f86cca82e634
evliess <jiajia_yuwei@163.com>
2019-06-16 19:05:41 +0800

    Springboot hystrix

--- SpringBoot/springboot-microservice-handon/README.md
+++ SpringBoot/springboot-microservice-handon/README.md
@@ -1,4 +1,22 @@
-# Springboot Microservice
+# Springboot Microservice With Vue
+
+
+## VUE Environment Configuration
+
+```
+//1. 设置淘宝镜像
+npm config set registry http://registry.npm.taobao.org/
+
+//2. 全局安装nuxt [参考](https://nuxtjs.org/guide/installation)
+npm i nuxt -g
+npm i nuxt --save
+
+//3. 使用npx创建项目
+npx create-nuxt-app <project-name>
+
+
+```
+
 
 ## Spring Reactive Framework
 
--- SpringBoot/springboot-microservice-handon/src/test/java/com/wait/play/ReactorTestCase.java
+++ SpringBoot/springboot-microservice-handon/src/test/java/com/wait/play/ReactorTestCase.java
@@ -1,5 +1,6 @@
 package com.wait.play;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -7,8 +8,10 @@ import java.util.List;
 import org.junit.Test;
 
 import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 public class ReactorTestCase {
+
   private List<String> words = Arrays
       .asList("the", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog");
 
@@ -21,5 +24,19 @@ public class ReactorTestCase {
     result.forEach(System.out::println);
   }
 
+  @Test
+  public void flapMap() {
+    StepVerifier.create(
+        Flux.just("flux", "mono").flatMap(s -> Flux.fromArray(s.split("\\s*")).delayElements(
+            Duration.ofMillis(100)))
+            .doOnNext(System.out::print)).expectNextCount(8).verifyComplete();
+
+  }
+
+  @Test
+  public void filter() {
+    StepVerifier.create(Flux.range(1, 6).filter(i -> i % 2 == 1).map(i -> i * i))
+        .expectNext(1, 9, 25).verifyComplete();
+  }
 
 }
