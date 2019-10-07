7809263910700c9d0c7d6c40349ce3e41f552412
JiaJiaGui <shguijj@cn.ibm.com>
2019-10-07 18:49:52 +0800

    python

--- Python3/soufangwang/fangtianxia.py
+++ Python3/soufangwang/fangtianxia.py
@@ -1,7 +1,10 @@
 import requests
 import json
+from pyquery import PyQuery as pq
+from selenium import webdriver
 from datetime import datetime
 from requests.exceptions import HTTPError
+from fileutil import append
 
 __city = 'sh'
 __years = 3
@@ -48,6 +51,44 @@ def getAreaPrice(areaCode):
         print(f'Other error occurred: {err}')
     else:
         print('Success!')
+    
+
+def getChengJiaoByName(name):
+    url = "https://yuanxierju.fang.com/chengjiao/"
+    geckodriverPath = r'C:/Users/JiaJiaGui/Desktop/New folder/PYTHON/geckodriver.exe'
+    
+    try:
+        driver = webdriver.Firefox(executable_path=geckodriverPath)
+        driver.get(url)
+        element = driver.find_element_by_id("ctl00_hlk_next")
+        count = 0
+        result = ''
+        split = 0
+        if(element):
+            while(element):
+                html = driver.page_source
+                d = pq(html)
+                if(count == 0):
+                    th = d('div.dealSent table tr th').items()
+                    for i in th:
+                        result += str(i.text()) + ','
+                    result = result[:-1]
+                    result += '\n'
+                td = d('div.dealSent table tr td').items()
+                for j in td:
+                    split = split + 1
+                    result += str(j.text()) + ','
+                    if(split > 0 and split % 5 == 0):
+                        result += '\n'
+                count = count + 1
+                element.click()
+    finally:
+        driver.close()
+        append('./data/data.txt', result)
+    
+    
+    
+
 
 #return 2019-10
 def covertTimestamp2Str(timestamp):
