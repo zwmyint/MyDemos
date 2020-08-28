236f183084efab58c825fd586a4a0521cf1ed48a
Jia Jia Gui <shguijj@cn.ibm.com>
2020-08-28 14:54:20 +0800

    add jquery validate demo

---
+++ jquery-validate-demo/usr.js
@@ -0,0 +1,47 @@
+$(function() {
+  // Initialize form validation on the registration form.
+  // It has the name attribute "registration"
+  
+  $("form[name='registration']").validate({
+    ignore: [],
+    // Specify validation rules
+    rules: {
+      // The key name on the left side is the name attribute
+      // of an input field. Validation rules are defined
+      // on the right side
+      firstname: "required",
+      lastname: "required",
+      email: {
+        required: true,
+        // Specify that email should be validated
+        // by the built-in "email" rule
+        email: true
+      },
+      password: {
+        required: true,
+        minlength: 5
+      },
+      hidenName: "required"
+    },
+    // Specify validation error messages
+    messages: {
+      firstname: "Please enter your firstname",
+      lastname: "Please enter your lastname",
+      password: {
+        required: "Please provide a password",
+        minlength: "Your password must be at least 5 characters long"
+      },
+      email: "Please enter a valid email address",
+      hidenName: "Please enter your hidenName!!!"
+    },
+    // Make sure the form is submitted to the destination defined
+    // in the "action" attribute of the form when valid
+    invalidHandler: function() {
+      console.log("1111111111111");
+    },
+    submitHandler: function(form) {
+      console.log("------------");
+      //form.submit();
+    }
+  });
+});
